ae59279d65cce81ab6bb2b65b1d7c951475d6624
Edo <i@edo.me>
2016-04-01 16:13:20 +0200

    Add nvm

--- install.sh
+++ install.sh
@@ -43,6 +43,9 @@ fi
 ./brew.sh
 ./brew-cask.sh
 
+# install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+
 ##############################################################################################################
 ### remaining configuration
 ###
