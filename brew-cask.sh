b46737d7d487bc854370ff47f2f5b88f9658fa6d
Edo <i@edo.me>
2017-01-09 11:25:04 +0100

    Update brew-cask.sh

--- brew-cask.sh
+++ brew-cask.sh
@@ -19,13 +19,8 @@ brew cask install github-desktop
 brew cask install sourcetree
 brew cask install balsamiq-mockups
 brew cask install textmate
-brew cask install transmit
 brew cask install virtualbox
-brew cask install vagrant
-brew cask install vagrant-manager
-brew cask install adobe-creative-cloud
 brew cask install sketch
-brew cask install dockertoolbox
 brew cask install ngrok
 
 # browsers
@@ -37,15 +32,9 @@ brew cask install google-chrome-canary
 # less often
 brew cask install disk-inventory-x
 brew cask install vlc
-brew cask install chitchat
 brew cask install numi
-brew cask install harvest
 brew cask install appcleaner
-brew cask install paparazzi
 brew cask install nvalt
-brew cask install soundnode
 brew cask install spotify
 brew cask install slack
 brew cask install skype
-brew cask install handbrake
-brew cask install crashplan
